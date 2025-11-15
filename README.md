7c7de4ee7b42557679cafe69b4df7bce55b7aba3
Matei Pralea <matei@machtentfaltung.de>
2025-11-15 17:38:09 +0200

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+  
+### *The source code for my website.*
+
+</div>
+
+## Building
+
+```bash
+$ trunk build --release
+```
+After building, the resulting files will be placed in the `dist/` folder.
+
+## Running for development
+
+```bash
+$ trunk serve
+```
+When the local server is running, you can access the website at the address:
+```
+http://localhost:8080/index.html#dev
+```
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+// Copyright © 2025 Matei Pralea <matei@machtentfaltung.de>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+#![warn(clippy::all)]
+#[allow(unused_must_use)]
+mod app;
+mod lang;
+
+use app::Application;
+
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = eframe::web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("main_canvas")
+            .expect("Failed to find main_canvas")
+            .dyn_into::<eframe::web_sys::HtmlCanvasElement>()
+            .expect("main_canvas was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(Application::new(cc)))),
+            )
+            .await;
+
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
+}
+
